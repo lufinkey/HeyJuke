a66941b55499db4f358e233eb4c98a54b85c550d
Luis Finke <lfinke@fusionalliance.com>
2019-01-20 13:47:56 -0500

    web socket server

--- server/src/Server.js
+++ server/src/Server.js
@@ -1,24 +1,79 @@
 
 const express = require('express');
+const WebSocket = require('ws');
+
+
+const WEB_SERVER_PORT = 8085;
+const WEB_SOCKET_PORT = 8086;
+
 
 class Server {
 	constructor(options={}) {
 		this._options = {...options};
+
+		this._webServer = null;
+		this._webSocketServer = null;
+		this._webSocket = null;
 	}
 
-	get port() {
-		if(this._options.port == null) {
-			return 6969;
+	get webServerPort() {
+		return this._options.webServerPort || WEB_SERVER_PORT;
+	}
+
+	get webSocketPort() {
+		return this._options.webSocketPort || WEB_SOCKET_PORT;
+	}
+
+	async _startWebSocketServer() {
+		if(this._webSocketServer != null) {
+			throw new Error("web socket server has already started");
 		}
-		return this._options.port;
+		let listening = false;
+		const webSocketServer = new WebSocket.Server({
+			port: this.webSocketPort
+		});
+		this._webSocketServer = webSocketServer;
+		webSocketServer.on('connection', (webSocket) => {
+			if(this._webSocket != null) {
+				webSocket.close();
+				return;
+			}
+			this._webSocket = webSocket;
+			webSocket.on('error', (error) => {
+				onSocketError(error);
+			});
+			webSocket.on('message', (message) => {
+				onSocketMessage(message);
+			});
+			webSocket.on('close', (code, reason) => {
+				console.log("web socket closed: "+code+": "+reason);
+				this._webSocket = null;
+			});
+		});
+		webSocketServer.on('listening', () => {
+			listening = true;
+		});
+		webSocketServer.on('error', () => {
+			if(!listening) {
+				this._webSocketServer = null;
+				this._webSocketServer.close();
+			}
+		});
+		webSocketServer.on('close', () => {
+			this._webSocketServer = null;
+		});
 	}
 
-	async start() {
-		const server = express();
-
+	async _startWebServer() {
+		if(this._webServer != null) {
+			throw new Error("web server has already started");
+		}
+		const webServer = express();
+		this._webServer = webServer;
 		await new Promise((resolve, reject) => {
-			server.listen(this.port, (error) => {
+			webServer.listen(this.port, (error) => {
 				if(error) {
+					this._webServer = null;
 					reject(error);
 				}
 				else {
@@ -26,6 +81,19 @@ class Server {
 				}
 			});
 		});
+	}
+
+	async start() {
+		await this._startWebSocketServer();
+		await this._startWebServer();
+	}
+
+	onSocketError(error) {
+		// TODO handle electron error
+	}
+
+	onSocketMessage(message) {
+		// TODO handle electron message
 	}
 }
 
